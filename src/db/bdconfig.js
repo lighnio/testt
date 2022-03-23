70eb48fc9ea19f85961d1ea88b65a12a426e62d0
lighnio <charlesvas16@outlook.com>
2022-03-23 00:00:54 -0600

    added dotenv and db config

---
+++ src/db/bdconfig.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const sqlConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: 'localhost',
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: false // change to true for local dev / self-signed certs
+  }
+}
+
+module.exports = sqlConfig;
